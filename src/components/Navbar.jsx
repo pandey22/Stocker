b12a19b28f7d057597148b77694539311562491c
Kshitij Pandey <you@example.comkshitijpandey25@gmail.com>
2025-01-11 15:06:11 +0530

    Added Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+export const Navbar = ()=>{
+    return <div>  
+        <nav className="mt-10 flex justify-center flex space-x-10 font-sans text-2xl">
+            <div>Home</div>
+            <div>About</div>
+            <div>Pricing</div>
+            <div>Login</div>
+            <div>SignUp</div>
+        </nav>
+
+    </div>
+}
